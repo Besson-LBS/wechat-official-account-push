d322232274ab30d0b162b828baa3dc8d56649a0a
青蛙军曹 <1466981275@qq.com>
2022-08-25 16:33:56 +0800

    六

--- src/main/java/cn/cvzhanshi/wechatpush/WechatpushApplication.java
+++ src/main/java/cn/cvzhanshi/wechatpush/WechatpushApplication.java
@@ -25,11 +25,11 @@ public class WechatpushApplication {
         log.info("发送成功");
     }
 
-    @Scheduled(cron = "0/5 * * * * ?")
-    public void goodMorning1() {
-//        Pusher.push("o-WcX6op8NCLS3z3J8Ez1JpBIwxA", "440305");//我
-//        Pusher.push("o-WcX6oTb5P_kKh4YNv0CRBWins4", "510100");//张志凯
-        log.info("每五秒发送一次");
-    }
+//    @Scheduled(cron = "0/5 * * * * ?")
+//    public void goodMorning1() {
+////        Pusher.push("o-WcX6op8NCLS3z3J8Ez1JpBIwxA", "440305");//我
+////        Pusher.push("o-WcX6oTb5P_kKh4YNv0CRBWins4", "510100");//张志凯
+//        log.info("每五秒发送一次");
+//    }
 
 }
