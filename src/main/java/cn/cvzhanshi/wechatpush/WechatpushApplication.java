e3f93717046eb6661fad17279afd3148db8ade1c
青蛙军曹 <1466981275@qq.com>
2022-08-26 18:46:42 +0800

    九

--- src/main/java/cn/cvzhanshi/wechatpush/WechatpushApplication.java
+++ src/main/java/cn/cvzhanshi/wechatpush/WechatpushApplication.java
@@ -14,9 +14,9 @@ public class WechatpushApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WechatpushApplication.class, args);
-//        Pusher.push("o-WcX6op8NCLS3z3J8Ez1JpBIwxA", "440305");//我
         Pusher.push("o-WcX6op8NCLS3z3J8Ez1JpBIwxA", "440305");//我
-        Pusher.push("o-WcX6oTb5P_kKh4YNv0CRBWins4", "510100");//张志凯
+//        Pusher.push("o-WcX6op8NCLS3z3J8Ez1JpBIwxA", "440305");//我
+//        Pusher.push("o-WcX6oTb5P_kKh4YNv0CRBWins4", "510100");//张志凯
     }
 
 
