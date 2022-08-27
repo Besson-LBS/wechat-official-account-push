8ea541b2b1289614b9d07e6e87b18c2cc876e63d
青蛙军曹 <1466981275@qq.com>
2022-08-27 08:22:01 +0800

    十

--- src/main/java/cn/cvzhanshi/wechatpush/WechatpushApplication.java
+++ src/main/java/cn/cvzhanshi/wechatpush/WechatpushApplication.java
@@ -14,18 +14,18 @@ public class WechatpushApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WechatpushApplication.class, args);
-        Pusher.push("o-WcX6op8NCLS3z3J8Ez1JpBIwxA", "440305");//我
-        Pusher.push("o-WcX6oTb5P_kKh4YNv0CRBWins4", "510100");//张志凯
+//        Pusher.push("o-WcX6op8NCLS3z3J8Ez1JpBIwxA", "440305");//我
+//        Pusher.push("o-WcX6oTb5P_kKh4YNv0CRBWins4", "510100");//张志凯
     }
 
 
 //    @Scheduled(cron = "0 20 7 1/1 * ?")
-//    @Scheduled(cron = "0 15,16,17,18,19,20 8 * * ? ")
-//    public void goodMorning() {
-//        Pusher.push("o-WcX6op8NCLS3z3J8Ez1JpBIwxA", "440305");//我
-////        Pusher.push("o-WcX6oTb5P_kKh4YNv0CRBWins4", "510100");//张志凯
-//        log.info("发送成功");
-//    }
+    @Scheduled(cron = "0 23,24,25,26,27,28,29 0,8 * * ? ")
+    public void goodMorning() {
+        Pusher.push("o-WcX6op8NCLS3z3J8Ez1JpBIwxA", "440305");//我
+//        Pusher.push("o-WcX6oTb5P_kKh4YNv0CRBWins4", "510100");//张志凯
+        log.info("发送成功");
+    }
 
     @Scheduled(cron = "0/5 * * * * ?")
     public void goodMorning1() {
